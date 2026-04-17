795a36f9509f62b8081d26eb30091ac443f6e1fb
FranciscoJavierSV <deriker.denoter@gmail.com>
2026-04-17 15:12:37 -0600

    cambios finales

--- front/src/app/components/featured/featured.ts
+++ front/src/app/components/featured/featured.ts
@@ -12,17 +12,17 @@ import { Card } from '../card/card';
 export class Featured {
   featuredCards = [
     {
-      imgSrc: '/assets/destacados/first.webp',
+      imgSrc: '/assets/carrusel/first.webp',
       title: 'Horizonte orbital',
       description: 'Un diseño que mezcla luz y sombra para una decoración con estilo espacial.',
     },
     {
-      imgSrc: '/assets/destacados/second.jpg',
+      imgSrc: '/assets/carrusel/second.jpg',
       title: 'Nebulosa dorada',
       description: 'Un cuadro que invita a explorar el universo desde un punto de vista íntimo.',
     },
     {
-      imgSrc: '/assets/destacados/third.jpg',
+      imgSrc: '/assets/carrusel/third.jpg',
       title: 'Ventana al espacio',
       description: 'Color y movimiento para una decoración impactante y moderna.',
     },
